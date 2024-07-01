3d78425c5ef7a11fbb641c93845cf53cf91e2129
invonzzz <serezha.simonin@yandex.ru>
2024-07-01 13:51:17 +0300

    roflik runner

---
+++ Assets/roflik/PlayerController.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private CharacterController controller;
+    private Vector3 dir;
+    [SerializeField] private int speed;
+    [SerializeField] private float jumpForce;
+    [SerializeField] private float gravity;
+
+    private int lineToMove = 1;
+    public float lineDistance = 4; 
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            if (lineToMove < 2)
+                lineToMove++;
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            if (lineToMove > 0)
+                lineToMove--;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (controller.isGrounded)
+                Jump();
+        }
+
+        Vector3 targetPosition = transform.position.z * transform.forward + transform.position.y * transform.up;
+        if (lineToMove == 0)
+            targetPosition += Vector3.left * lineDistance;
+        else if (lineToMove == 2)
+            targetPosition += Vector3.right * lineDistance;
+
+        transform.position = targetPosition;
+    }
+
+    private void Jump()
+    {
+        dir.y = jumpForce;
+    }
+
+    void FixedUpdate()
+    {
+        dir.z = speed;
+        dir.y += gravity * Time.fixedDeltaTime;
+        controller.Move(dir * Time.fixedDeltaTime);
+    }
+}
