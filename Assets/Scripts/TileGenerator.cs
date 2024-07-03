e3bbaa62d180f3fd1d22f17ab1821026c3ff6bf3
et3rnalpain <gjobovi4@mail.ru>
2024-07-03 22:13:59 +0300

    Added generation and posteffect

--- Assets/Scripts/TileGenerator.cs
+++ Assets/Scripts/TileGenerator.cs
@@ -6,23 +6,42 @@ public class TileGenerator : MonoBehaviour
 {
 
     public GameObject[] prefabs;
+    public GameObject player;
+    public int startcount = 2;
+    private List<GameObject> tiles = new List<GameObject>();
     public int spawnpos = 0;
     // Start is called before the first frame update
     void Start()
     {
-        Spawn(0);
-        Spawn(1);
+        for(int i = 0; i < startcount; i++)
+        {
+            Spawn(Random.Range(0,prefabs.Length));
+
+        }
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if(player.transform.position.z > spawnpos - (startcount*50))
+        {
+            Spawn(Random.Range(0,prefabs.Length));
+            Delete();
+        }
     }
 
     private void Spawn(int index)
     {
-        Instantiate(prefabs[index], transform.forward * spawnpos, transform.rotation);
-        spawnpos += 155;
+        Vector3 newpos = gameObject.transform.position;
+        newpos.z+=spawnpos;
+        GameObject obj = Instantiate(prefabs[index],newpos, gameObject.transform.rotation);
+        tiles.Add(obj);
+        spawnpos += 154;
+    }
+
+    private void Delete()
+    {
+        Destroy(tiles[0]);
+        tiles.RemoveAt(0);
     }
 }
